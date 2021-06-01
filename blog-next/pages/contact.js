a70137089255a32527b88fbd4d1416b044d29b9d
lkmSasanga <lkmsasanga@gmail.com>
2021-06-01 22:56:15 +0530

    about & contact pages added

---
+++ blog-next/pages/contact.js
@@ -0,0 +1,10 @@
+import Layout from "../components/layout/Layout";
+
+export default function Contact() {
+    return ( <
+        Layout >
+        <
+        div > Contact Page < /div> <
+        /Layout>
+    );
+}
