3962327fb02b31472586f33161e487933af19ad8
lkmSasanga <lkmsasanga@gmail.com>
2021-06-01 21:53:02 +0530

    nav bar added

--- blog-next/pages/index.js
+++ blog-next/pages/index.js
@@ -1,21 +1,24 @@
+import Layout from "../components/layout/Layout";
 import Link from "next/link";
 
 export default function Home({ posts }) {
   console.log("i am on the client");
 
   return (
-    <div>
-      {/* loop over the posts and show */}
-      {posts &&
-        posts.map((post) => (
-          <Link href={`/${post.Slug}`} key={post.id}>
-            <a>
-              <h2>{post.Title}</h2>
-              <div>{post.User.username}</div>
-            </a>
-          </Link>
-        ))}
-    </div>
+    <Layout>
+      <div>
+        {/* loop over the posts and show */}
+        {posts &&
+          posts.map((post) => (
+            <Link href={`/${post.Slug}`} key={post.id}>
+              <a>
+                <h2>{post.Title}</h2>
+                <div>{post.User.username}</div>
+              </a>
+            </Link>
+          ))}
+      </div>
+    </Layout>
   );
 }
 
