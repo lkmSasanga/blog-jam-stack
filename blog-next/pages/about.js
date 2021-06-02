27bcfc13187e34a77b206bf1c587a3a5eff8baa8
lkmSasanga <lkmsasanga@gmail.com>
2021-06-02 09:19:12 +0530

    about page edited

--- blog-next/pages/about.js
+++ blog-next/pages/about.js
@@ -3,7 +3,7 @@ import Layout from "../components/layout/Layout";
 export default function About() {
   return (
     <Layout>
-      <div>About Page</div>
+      <div> About Page </div>{" "}
     </Layout>
   );
 }
